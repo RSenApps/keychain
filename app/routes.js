926ab0918043061221420ce3f040a9c5e7e6e1d0
Sarah Wooders <sarahwooders@gmail.com>
2018-01-21 00:14:35 -0500

    ok version of webapp

--- app/routes.js
+++ app/routes.js
@@ -59,7 +59,7 @@ module.exports = function(app, passport) {
         failureFlash : true // allow flash messages
     }));
 
-    app.post('/login', passport.authenticate('local-login', {
+    app.post('/login', passport.authenticate('local', {
         successRedirect : '/profile', // redirect to the secure profile section
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
@@ -70,10 +70,26 @@ module.exports = function(app, passport) {
 function isLoggedIn(req, res, next) {
 
     // if user is authenticated in the session, carry on 
-    if (req.isAuthenticated())
-        return next();
+    var contract = web3.eth.contract(interface);
+    var instance = contract.at(contractAddress);
+    console.log("HELLO");
+    var email = req["user"]["local"]["email"];
+    instance.Query_access.call("bank", email, callback=function(err, result) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log('Query result:');
+            console.log(result);
 
-    // if they aren't redirect them to the home page
-    res.redirect('/');
+            if(result) {
+                return next();
+            }
+
+            //User.findOne({ 'local.email' :  email }, function(err, user) {
+            //    return done(null, user);
+            //});
+        }
+    });
+ 
 }
 
