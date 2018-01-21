35c37a84a5a6d6e975f35414c6615933ada3ecd0
Ryan Senanayake <rsenapps@gmail.com>
2018-01-20 21:40:37 -0500

    before fingerprint

--- Android/app/src/main/java/keychain/com/keychain/MyFirebaseInstanceIdService.java
+++ Android/app/src/main/java/keychain/com/keychain/MyFirebaseInstanceIdService.java
@@ -1,11 +1,23 @@
 package keychain.com.keychain;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 /**
  * Created by rsen on 1/20/18.
@@ -24,7 +36,30 @@ public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
 
         SharedPreferences sharedPref = getSharedPreferences("prefs", Context.MODE_PRIVATE);
         if (sharedPref.getString("username", null) != null) {
-            //TODO: registerTokenWithServer
+            RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+            String url = "https://l6g44s18fj.execute-api.us-east-1.amazonaws.com/prod/"; //FILL API register
+
+            try {
+                url += "username=" + URLEncoder.encode(sharedPref.getString("username", null), "UTF-8") + "&UID=" + URLEncoder.encode(refreshedToken, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            // Request a string response from the provided URL.
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Toast.makeText(getApplicationContext(), "Refreshed UID with server", Toast.LENGTH_SHORT).show();
+                            //Go to next activity
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), "Failed to refresh UID with server", Toast.LENGTH_LONG).show();
+                }
+            });
+            queue.add(stringRequest);
         }
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putString("firebase_token", refreshedToken);
