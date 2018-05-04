0499161c3e9dd2900d26a51ec80ac723e6f078f9
fervorarc <mshumikhin@gmail.com>
2018-05-03 23:45:19 -0400

    Update KeychainIdentity.sol

--- KeychainIdentity.sol
+++ KeychainIdentity.sol
@@ -1,30 +1,30 @@
 pragma solidity ^0.4.23;
 
 contract KeychainIdentity {
-
+  
   mapping(bytes32 => bool) user_set;
   mapping(bytes32 => uint8) user_to_recovery_threshold;
   mapping(bytes32 => bytes32[]) user_to_recovery_users;
-  mapping(bytes32 => address) user_to_key;
+  mapping(bytes32 => address[]) user_to_keys;
   mapping(bytes32 => mapping(bytes32 => address)) recovery_users_approval;
   mapping(bytes32 => mapping(bytes32 => bool)) is_recovery_user;
-  mapping(bytes32 => mapping(address => uint24)) user_to_past_keys;
-  mapping(bytes32 => uint24) past_keys_version;
+  mapping(bytes32 => mapping(address => uint24)) valid_keys;
+  mapping(bytes32 => uint24) current_keys_version;
 
   // Setup
   function Create_username(bytes32 username, address key) public returns (bool) {
     require(!user_set[username]);
     user_set[username] = true;
-    user_to_key[username] = key;
-    user_to_past_keys[username][key] = 0;
-    past_keys_version[username] = 0;
+    user_to_keys[username].push(key);
+    valid_keys[username][key] = 0;
+    current_keys_version[username] = 0;
     return true;
   }
   
   function Create_web_of_trust(bytes32 username, bytes32[] username_list, uint8 size, uint8 threshold) public returns (bool) {
-    require(user_to_key[username] == msg.sender);
+    require(valid_keys[username][msg.sender] == current_keys_version[username]);
     require(user_to_recovery_users[username].length < 1);
-    require(size < 10);
+    require(size < 17);
     require(threshold <= size);
     require(threshold >= 1);
     for (uint8 i = 0; i < size; i++) {
@@ -36,16 +36,16 @@ contract KeychainIdentity {
   }
 
   // Updates
-  function Do_change_key(bytes32 username, address new_key) public returns (bool) {
-    require(user_to_key[username] == msg.sender);
-    user_to_past_keys[username][msg.sender] = past_keys_version[username];
-    user_to_past_keys[username][new_key] = past_keys_version[username];
-    user_to_key[username] = new_key;
+  function Do_add_key(bytes32 username, address new_key) public returns (bool) {
+    require(valid_keys[username][msg.sender] == current_keys_version[username]);
+    require(user_to_keys[username].length < 17);
+    valid_keys[username][new_key] = current_keys_version[username];
+    user_to_keys[username].push(new_key);
     return true;
   }
   
   function Do_recover_address(bytes32 username, bytes32 recovery_user, address new_address) public returns (bool) {
-    require(user_to_key[recovery_user] == msg.sender);
+    require(valid_keys[recovery_user][msg.sender] == current_keys_version[recovery_user]);
     require(is_recovery_user[username][recovery_user] == true);
     recovery_users_approval[username][recovery_user] = new_address;
     return true;
@@ -59,8 +59,8 @@ contract KeychainIdentity {
         }
     }
     if (recov_ct >= user_to_recovery_threshold[username]) {
-        past_keys_version[username] += 1;
-        user_to_key[username] = new_address;
+        current_keys_version[username] += 1;
+        user_to_keys[username].push(new_address);
         return 0;
     } else {
         return user_to_recovery_threshold[username] - recov_ct;
@@ -68,8 +68,8 @@ contract KeychainIdentity {
   }
   
   function Do_revoke(bytes32 username) public returns (bool) {
-    require(user_to_past_keys[username][msg.sender] == past_keys_version[username]);
-    user_to_key[username] = address(0);
+    require(valid_keys[username][msg.sender] == current_keys_version[username]);
+    delete user_to_keys[username];
     return true;
   }
 
@@ -78,8 +78,9 @@ contract KeychainIdentity {
     return user_set[username];
   }
   
-  function Query_user_key(bytes32 username) public view returns (address) {
-    return user_to_key[username];
+  function Query_user_keys(bytes32 username) public view returns (address[]) {
+    address[] storage res = user_to_keys[username];
+    return res;
   }
   
   function Query_recovery_users(bytes32 username) public view returns (bytes32[]) {
